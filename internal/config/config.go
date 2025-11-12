74b1821dce537dfa4b48aa06610e890886de8a5a
arpanbht <arpanbht.ofc@gmail.com>
2025-11-12 11:30:51 +0530

    feat: implement MustLoad function to handle configuration loading and path resolution

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type HTTPServer struct {
 	Addr string
 }
@@ -11,4 +19,30 @@ type Configuration struct {
 	Env         string `yaml:"env" env:"ENV" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server" env-required:"true"`
+}
+
+func MustLoad() *Configuration {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist at path: %s", configPath)
+	}
+
+	var cfg Configuration
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Failed to read config: %s", err.Error())
+	}
+
+	return &cfg
 }
